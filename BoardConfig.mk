e93a0b5ba1dcdf37a3e3ce03a29719d85fd8ece7
FengGuan <167394962+fengguan9527@users.noreply.github.com>
2026-07-10 00:27:05 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -240,7 +240,7 @@ TW_MAX_BRIGHTNESS        := 4094
 TW_NO_SCREEN_BLANK  := true
 TW_SCREEN_BLANK_ON_BOOT  := true
 TW_Y_OFFSET              := 0
-TW_H_OFFSET              := -141
+TW_H_OFFSET              := 141
 TW_STATUS_ICONS_ALIGN    := center
 
 
